7eb1c488ce2bbdcdb5aa1884597fb86e0ee492aa
Baumerx20 <bashript@gmail.com>
2024-04-23 13:01:06 -0600

    Committing

---
+++ PenTools/nslookup.py
@@ -0,0 +1,19 @@
+import argparse
+import socket
+
+parser = argparse.ArgumentParser()
+parser.add_argument("domain", help="python nslookup.py www.domain.com")
+args = parser.parse_args()
+print(args.domain)
+
+class NetworkUtilites:
+    def nslookup(self, domain):
+        
+        nslookup_result_ip = socket.gethostbyname(domain)
+        print(nslookup_result_ip)
+
+def run() -> None:
+    network_utilities = NetworkUtilites()
+    network_utilities.nslookup(args.domain)
+
+run()
