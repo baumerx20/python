3b37d49c48d841c23b250d1bd577ab5b69056547
Baumerx20 <bashript@gmail.com>
2024-05-20 14:03:38 -0600

    removed

--- Parsers/Csv_extractor.py
+++ Parsers/Csv_extractor.py
@@ -1,5 +1,7 @@
 import csv
 
+#Starter for examining output files
+
 def extract_columns_and_rows(input_file, output_file, columns_to_extract, rows_to_extract):
     with open(input_file, 'r') as infile, open(output_file, 'w', newline='') as outfile:
         reader = csv.DictReader(infile)
@@ -13,7 +15,6 @@ def extract_columns_and_rows(input_file, output_file, columns_to_extract, rows_t
 
 input_file = 'large_file.csv'
 output_file = 'extracted_data.csv'
-columns_to_extract = ['column1', 'column2', 'column3']  # specify the columns you want to extract
-rows_to_extract = ['value1', 'value2']  # specify the rows you want to extract
+columns_to_extract = ['column1', 'column2', 'column3'] 
 
 extract_columns_and_rows(input_file, output_file, columns_to_extract, rows_to_extract)
